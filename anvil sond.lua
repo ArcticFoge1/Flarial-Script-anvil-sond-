3b86ffa78ddce57cec793545f930e2d2ce3aebbf
ArcticFoge1 <WyattPhilyaw@outlook.com>
2025-05-15 16:57:31 -0500

    Add files via upload

---
+++ anvil sond.lua
@@ -0,0 +1,8 @@
+name = "Play Sound on Win"
+description = "Plays a sound when 'You killed' is said in chat"
+
+onEvent("ChatReceiveEvent", function(message, name, type)
+    if string.find(message, "You killed") then
+        audio.play("Scripts/anvil-drop-faster 70% valum.wav")
+    end
+end)
